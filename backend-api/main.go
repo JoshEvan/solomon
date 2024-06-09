104339d26332fc183443e17629252ea599e4db9d
joshua.arijanto <joshua.arijanto@tokopedia.com>
2024-06-09 12:24:56 +0700

    feat(binary): Update binary init

--- backend-api/main.go
+++ backend-api/main.go
@@ -4,9 +4,12 @@ import (
 	"github.com/JoshEvan/solomon/controller/http"
 	"github.com/JoshEvan/solomon/driver/config"
 	"github.com/JoshEvan/solomon/driver/net"
+	"github.com/JoshEvan/solomon/driver/util"
 )
 
 func main() {
+	defer util.PanicCapture()
+
 	httpRouter := net.NewRouter()
 	http.InitHTTPHandler(httpRouter, config.Get())
 	err := net.ServeHTTP(httpRouter.GetHTTPHandler(), ":9099")
